48a24ab1eedc2e2805683e51be3ce54ec1aab819
Luis Pinto <mail@lpinto.eu>
2017-10-04 10:48:14 +0100

    token entity table name

--- src/main/java/eu/lpinto/universe/persistence/entities/Token.java
+++ src/main/java/eu/lpinto/universe/persistence/entities/Token.java
@@ -9,6 +9,7 @@ import javax.persistence.*;
  * @author Luis Pinto <code>- mail@lpinto.eu</code>
  */
 @Entity
+@Table(name = "Token")
 public class Token implements Serializable {
 
     private static final long serialVersionUID = 1L;
