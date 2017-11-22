155cbc290c627a226b49d91ffe28aa1af9023abc
Luís Pinto <mail@lpinto.eu>
2017-11-22 10:05:54 +0000

    fix imports

--- src/main/java/eu/lpinto/universe/api/services/Tokens.java
+++ src/main/java/eu/lpinto/universe/api/services/Tokens.java
@@ -1,23 +1,16 @@
 package eu.lpinto.universe.api.services;
 
+import eu.lpinto.universe.api.dto.Errors;
 import eu.lpinto.universe.api.dts.UserDTS;
 import eu.lpinto.universe.controllers.TokenController;
+import eu.lpinto.universe.controllers.exceptions.PreConditionException;
 import eu.lpinto.universe.persistence.entities.Token;
 import eu.lpinto.universe.persistence.entities.User;
-import eu.lpinto.universe.api.dto.Errors;
-import eu.lpinto.universe.controllers.exceptions.PreConditionException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.HeaderParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
