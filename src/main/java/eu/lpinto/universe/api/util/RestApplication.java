017310de0065196427a0e04a6715c7a40f9515b0
Luis Pinto <mail@lpinto.eu>
2018-06-14 20:13:37 +0100

    add Rest Application

---
+++ src/main/java/eu/lpinto/universe/api/util/RestApplication.java
@@ -0,0 +1,13 @@
+package eu.lpinto.universe.api.util;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Root configuration of Jersey and the REST API.
+ *
+ * @author Luis Pinto <code>- luis.pinto@petuniversal.com</code>
+ */
+@ApplicationPath("api")
+public class RestApplication extends Application {
+}
