4599e917ee6e9ed259b70a88983d340d089fbb66
Luis Pinto <mail@lpinto.eu>
2018-07-28 21:41:38 +0100

    Update rest app confg

--- src/main/java/eu/lpinto/universe/api/util/RestApplication.java
+++ src/main/java/eu/lpinto/universe/api/util/RestApplication.java
@@ -1,13 +1,13 @@
 package eu.lpinto.universe.api.util;
 
+import eu.lpinto.universe.util.UniverseFundamentals;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 /**
- * Root configuration of Jersey and the REST API.
  *
  * @author Luis Pinto <code>- luis.pinto@petuniversal.com</code>
  */
-@ApplicationPath("api")
-public class RestApplication extends Application {
+@ApplicationPath(UniverseFundamentals.REST_BASE_URI)
+public class RestApplication extends javax.ws.rs.core.Application {
+
 }
