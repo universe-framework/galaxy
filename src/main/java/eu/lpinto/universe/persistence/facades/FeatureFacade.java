aeb0d14364e3a1685dbd8e896bfb44cfde17c040
Luis Pinto <mail@lpinto.eu>
2018-08-07 04:06:02 +0100

    find features by organization

--- src/main/java/eu/lpinto/universe/persistence/facades/FeatureFacade.java
+++ src/main/java/eu/lpinto/universe/persistence/facades/FeatureFacade.java
@@ -2,7 +2,8 @@ package eu.lpinto.universe.persistence.facades;
 
 import eu.lpinto.universe.persistence.entities.Feature;
 import eu.lpinto.universe.persistence.entities.PlanFeature;
-import eu.lpinto.universe.persistence.facades.AbstractFacade;
+import java.util.List;
+import java.util.Map;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -25,6 +26,25 @@ public class FeatureFacade extends AbstractFacade<Feature> {
     @Override
     protected EntityManager getEntityManager() {
         return em;
+    }
+
+    @Override
+    public List<Feature> find(Map<String, Object> options) {
+        if (options.containsKey("organization")) {
+
+            Long orgID = (Long) options.get("organization");
+
+            return getEntityManager().createQuery("SELECT f FROM Feature f"
+                                                  + " join PlanFeature pf ON pf.feature = f.id"
+                                                  + " join Plan p ON pf.plan.id = p.id"
+                                                  + " join Company c ON p.id = c.plan.id"
+                                                  + " join Organization o ON c.id = o.company.id where o.id = :orgID",
+                                                  Feature.class)
+                    .setParameter("orgID", orgID)
+                    .getResultList();
+        }
+
+        return super.find(options);
     }
 
     @Override
