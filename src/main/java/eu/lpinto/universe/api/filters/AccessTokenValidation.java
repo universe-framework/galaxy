5eb7d8247b257c592c4b0ce074e4002ff0a8fb07
Luis Pinto <mail@lpinto.eu>
2019-03-21 13:24:40 +0000

    take companies and organizations out of the dmz

--- src/main/java/eu/lpinto/universe/api/filters/AccessTokenValidation.java
+++ src/main/java/eu/lpinto/universe/api/filters/AccessTokenValidation.java
@@ -47,8 +47,6 @@ public class AccessTokenValidation implements ContainerRequestFilter, ContainerR
         DMZ_ENDPOINTS.put("/plans", HttpMethod.GET);
         DMZ_ENDPOINTS.put("/features", HttpMethod.GET);
         DMZ_ENDPOINTS.put("/planFeatures", HttpMethod.GET);
-        DMZ_ENDPOINTS.put("/companies", HttpMethod.GET);
-        DMZ_ENDPOINTS.put("/companies", HttpMethod.POST);
     }
 
     @Override
@@ -128,8 +126,7 @@ public class AccessTokenValidation implements ContainerRequestFilter, ContainerR
         }
 
         if (service[1].startsWith("/tokens/")
-            || service[1].startsWith("/invites")
-            || service[1].startsWith("/organizations/")) {
+            || service[1].startsWith("/invites")) {
             return true;
         }
 
