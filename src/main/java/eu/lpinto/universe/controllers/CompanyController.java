099733d6a296763b863d05456b4f8ee1a65b5c76
Luís Pinto <mail@lpinto.eu>
2019-10-29 12:42:42 +0000

    company is created with a plan defined by the properties file

--- src/main/java/eu/lpinto/universe/controllers/CompanyController.java
+++ src/main/java/eu/lpinto/universe/controllers/CompanyController.java
@@ -9,6 +9,7 @@ import eu.lpinto.universe.persistence.entities.Employee;
 import eu.lpinto.universe.persistence.entities.EmployeeProfile;
 import eu.lpinto.universe.persistence.entities.Plan;
 import eu.lpinto.universe.persistence.facades.CompanyFacade;
+import eu.lpinto.universe.util.UniverseFundamentals;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -38,7 +39,7 @@ public class CompanyController extends AbstractControllerCRUD<Company> {
     public void doCreate(final Long userID, final Map<String, Object> options, Company entity) throws PreConditionException, UnknownIdException, PermissionDeniedException {
 
         if (entity.getPlan() == null) {
-            entity.setPlan(new Plan(1l));
+            entity.setPlan(new Plan(Long.valueOf(UniverseFundamentals.DEFAULT_PLAN)));
         }
 
         super.doCreate(userID, options, entity);
