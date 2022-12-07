c168ce2066d91f61e8a60fa3bc5e488dd56061d2
Luís Pinto <luis.pinto@petuniversal.com>
2022-12-07 18:56:20 +0000

    remove print

--- src/main/java/eu/lpinto/universe/api/services/OrganizationService.java
+++ src/main/java/eu/lpinto/universe/api/services/OrganizationService.java
@@ -3,13 +3,9 @@ package eu.lpinto.universe.api.services;
 import eu.lpinto.universe.api.dto.OrganizationDTO;
 import eu.lpinto.universe.api.dts.OrganizationDTS;
 import eu.lpinto.universe.controllers.OrganizationController;
-import eu.lpinto.universe.controllers.exceptions.PermissionDeniedException;
-import eu.lpinto.universe.controllers.exceptions.PreConditionException;
 import eu.lpinto.universe.persistence.entities.Organization;
-import java.util.Map;
 import javax.ejb.EJB;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
 
 /**
  * REST CRUD service for Clinic.
@@ -18,12 +14,6 @@ import javax.ws.rs.core.Response;
  */
 @Path("organizations")
 public class OrganizationService extends AbstractServiceCRUD<Organization, OrganizationDTO, OrganizationController, OrganizationDTS> {
-
-    @Override
-    public Response doFind(Map<String, Object> options) throws PreConditionException, PermissionDeniedException {
-        System.out.println(options.get("remoteAddr"));
-        return super.doFind(options); //To change body of generated methods, choose Tools | Templates.
-    }
 
     @EJB
     private OrganizationController controller;
