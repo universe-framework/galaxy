a10eee04cb02747b2a60192970ad55821ef19f85
Luís Pinto <luis.pinto@petuniversal.com>
2023-02-13 19:02:27 +0000

    remove duplicate find on invites

--- src/main/java/eu/lpinto/universe/api/services/InviteService.java
+++ src/main/java/eu/lpinto/universe/api/services/InviteService.java
@@ -1,28 +1,10 @@
 package eu.lpinto.universe.api.services;
 
 import eu.lpinto.universe.api.dts.InviteDTS;
-import eu.lpinto.universe.api.util.StatusEmail;
 import eu.lpinto.universe.controllers.InviteController;
-import eu.lpinto.universe.controllers.exceptions.PermissionDeniedException;
-import eu.lpinto.universe.controllers.exceptions.PreConditionException;
 import eu.lpinto.universe.persistence.entities.Invite;
-import eu.lpinto.universe.util.UniverseFundamentals;
-import java.util.HashMap;
-import java.util.Map;
-import javax.ejb.Asynchronous;
 import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 /**
  * REST CRUD service for Invite.
@@ -37,48 +19,6 @@ public class InviteService extends AbstractServiceCRUD<Invite, eu.lpinto.univers
 
     public InviteService() {
         super(InviteDTS.T);
-    }
-
-    @GET
-    @Asynchronous
-    @Produces(value = MediaType.APPLICATION_JSON)
-    public void find(@Suspended final AsyncResponse asyncResponse,
-                     final @Context UriInfo uriInfo,
-                     final @Context HttpHeaders headers,
-                     final @QueryParam(value = "code") String code,
-                     final @HeaderParam(value = UniverseFundamentals.AUTH_USER_ID) Long userID) throws PreConditionException {
-        Map<String, Object> options = new HashMap<>(10 + uriInfo.getQueryParameters().size());
-
-        try {
-            /* Setup */
-            buildOptions(options, uriInfo, userID);
-
-            if (code != null) {
-                options.put("code", code);
-            }
-
-            /* Log request */
-            logRequest(uriInfo, options, currentMethod());
-
-            /* Body */
-            Response result = doFind(options);
-
-            /* Log response */
-            options.put("service.end", System.currentTimeMillis());
-            logResponse(uriInfo, headers, options, currentMethod(), result.getEntity());
-
-            /* return */
-            asyncResponse.resume(result);
-
-        } catch (PermissionDeniedException ex) {
-            getLogger().error(ex.getMessage(), ex);
-            StatusEmail.sendExceptionEmail(ex, uriInfo, headers, options);
-            asyncResponse.resume(forbidden((Long) options.get("user")));
-
-        } catch (RuntimeException ex) {
-            StatusEmail.sendExceptionEmail(ex, uriInfo, headers, options);
-            asyncResponse.resume(noContent());
-        }
     }
 
     @Override
